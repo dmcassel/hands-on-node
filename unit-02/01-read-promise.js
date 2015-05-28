8a92a732cf8a72d557d85afd1a1a86ede397b1da
Dave Cassel <dmcassel@gmail.com>
2015-05-28 10:56:28 -0400

    Adding unit 2: debugging

---
+++ unit-02/01-read-promise.js
@@ -0,0 +1,19 @@
+// Read a document from the database.
+// Handle the response using the Promise approach.
+
+var ml = require('marklogic');
+var conn = require('../config.js').connection;
+var db = ml.createDatabaseClient(conn);
+
+// document URI in the database
+var imgURI = '/image/01.JPG.json';
+
+db.documents.read(imgURI).result()
+  .then(function(docs) {
+    // write out the document
+    debugger;
+    console.log(docs[0]);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
---
+++ unit-02/README.md
@@ -0,0 +1,14 @@
+Unit 2 shows how to use Node.js's built-in debugging while working with 
+MarkLogic's Node Client API. 
+
+In this unit, we will cover:
+
+- Running in debug mode
+- [Node Inspector](https://github.com/node-inspector/node-inspector)
+  - `$ node-debug 01-read-promise.js`
+- setting breakpoints
+  - statically ("debugger")
+  - dynamically (sb)
+- debugger commands (next, step, out, run, cont, watch)
+- REPL
+- JSHint -- reminder to remove "debugger" statements
