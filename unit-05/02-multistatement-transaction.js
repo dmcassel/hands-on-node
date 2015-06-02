8b0bbf37317c44178171821f81b1f27e8c5d76b2
Dave Cassel <dmcassel@gmail.com>
2015-06-02 19:47:24 -0400

    Two corrections: get txid properly; wait for all promises before commit

--- unit-05/02-multistatement-transaction.js
+++ unit-05/02-multistatement-transaction.js
@@ -7,39 +7,51 @@ var pb = ml.patchBuilder;
 var qb = ml.queryBuilder;
 var bPromise = require('bluebird');
 
-var txid = db.transactions.open();
-
 var updates = [];
 
-db.documents.query(
-  qb.where(
-    qb.collection('image')
-  )
-  .slice(1, 1000, qb.extract('/created'))
-).stream()
-  .on('data', function(document) {
-    // process one document
-    // document.content.extracted[0].created
-    var date = new Date(document.content.extracted[0].created).toISOString().slice(0,10);
-    console.log(document.uri + ' was created on ' + date);
+// Create a transaction
+db.transactions.open().result().then(function(txid) {
+  // Search for all the JSON image descriptions
+  db.documents.query(
+    qb.where(
+      qb.collection('image')
+    )
+    .slice(1, 1000, qb.extract('/created'))
+  ).stream()
+    .on('data', function(document) {
+      // process one document
 
-    updates.push(db.documents.patch(
-      document.uri,
-      pb.insert(
-        '/created',
-        'after',
-        { createdDate: date }
-      ),
-      'content',
-      txid
-    ).result());
-  }).on('end', function() {
-    // all data received
-    bPromise.all(updates);
-    db.transactions.commit(txid);
-    console.log('All done.');
-  }).on('error', function(error) {
-    // handle errors
-    db.transactions.rollback(txid);
-    console.log('Something went wrong: ' + error);
-  });
+      // Convert the epoch to a date we can build an index on
+      var date = new Date(document.content.extracted[0].created).toISOString().slice(0,10);
+      console.log(document.uri + ' was created on ' + date);
+
+      updates.push(db.documents.patch(
+        document.uri,
+        pb.replaceInsert(
+          '/createdDate',
+          '/created',
+          'after',
+          { createdDate: date }
+        ),
+        'content',
+        txid
+      ).result());
+    }).on('end', function() {
+      // All JSON image descriptions received. Wait for updates to finish.
+      bPromise.all(updates)
+        .then(function() {
+          // All updates succeeded. Commit the transaction.
+          db.transactions.commit(txid);
+          console.log('All done.');
+        })
+        .catch(function(error){
+          // Something went wrong. Roll back the transaction.
+          db.transactions.rollback(txid);
+          console.log('Error: ' + error);
+        });
+    }).on('error', function(error) {
+      // Something went wrong. Roll back the transaction.
+      db.transactions.rollback(txid);
+      console.log('Problem reading documents: ' + error);
+    });
+});
