7571e8d550858b23e7624a994bffffce061150b8
Dave Cassel <dmcassel@gmail.com>
2015-06-02 19:56:07 -0400

    added a facet example to unit 3

---
+++ unit-03/07-facets.js
@@ -0,0 +1,24 @@
+// Search for documents using text and return facets.
+
+// Search for documents that contain 'apple' and return a facet with the
+// country.
+
+var ml = require('marklogic');
+var conn = require('../config.js').connection;
+var db = ml.createDatabaseClient(conn);
+var qb = ml.queryBuilder;
+
+db.documents.query(
+  qb.where(
+    qb.parsedFrom('apple')
+  )
+  .calculate(qb.facet('Country', qb.property('country'),
+    qb.facetOptions('limit=10', 'descending')))
+  .withOptions({metrics: true})
+).result()
+.then(function(docs) {
+  console.log('This search found: ' + JSON.stringify(docs));
+})
+.catch(function(error) {
+  console.log('something went wrong: ' + error);
+});
