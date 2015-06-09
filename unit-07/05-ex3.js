86762fb04d09b9ce3fcee9afbd6b7f4b22ee3182
Dave Cassel <dmcassel@gmail.com>
2015-06-09 10:19:04 -0400

    added skeleton implementation

--- unit-07/05-ex3.js
+++ unit-07/05-ex3.js
@@ -1,3 +1,19 @@
 // Do a search that applies the search transform.
 //
 // Hint: use qb.where(...).slice(qb.transform('transform-name', params))
+var ml = require('marklogic');
+var conn = require('../config.js').reader;
+var db = ml.createDatabaseClient(conn);
+var qb = ml.queryBuilder;
+
+db.documents.query(
+  // TODO: define the query and apply the transform
+
+).result()
+.then(function(docs) {
+  // TODO: print the results
+
+})
+.catch(function(error) {
+  console.log('something went wrong: ' + error);
+});
