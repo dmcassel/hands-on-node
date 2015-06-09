726b86282b650b215a56fece6ea163ed82fd32f6
Dave Cassel <dmcassel@gmail.com>
2015-06-09 10:04:13 -0400

    added skeleton implementation

--- unit-07/03-ex1.js
+++ unit-07/03-ex1.js
@@ -1,2 +1,21 @@
 // Write a transform that will work on search results, adding the binary image
 // size as metadata to each result.
+
+function addImageSizes(context, params, content)
+{
+  if (context.inputType.search('json') >= 0) {
+    var result = content.toObject();
+    if (result.hasOwnProperty('snippet-format')) {
+      // This is a metadata section
+
+    } else {
+      // This is one document
+      // TODO: calculate the size of the binary, add it to result
+
+    }
+  }
+
+  return content;
+}
+
+exports.transform = addImageSizes;
