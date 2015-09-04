3a975c9a7bdff49faf4a86299d5cc5187a39eac5
Dave Cassel <dmcassel@gmail.com>
2015-09-04 16:21:29 -0400

    Adding answers for units 01 and 03.

--- unit-03/09-ex-1.js
+++ unit-03/09-ex-1.js
@@ -6,3 +6,18 @@ var conn = require('../config.js').admin;
 var db = ml.createDatabaseClient(conn);
 var qb = ml.queryBuilder;
 
+db.documents.query(
+  qb.where(
+    qb.byExample({
+      make: 'LGE',
+      country: 'Colombia'
+    })
+  )
+  .withOptions({metrics: true})
+).result()
+.then(function(docs) {
+  console.log('This search found ' + docs[0].total + ' docs');
+})
+.catch(function(error) {
+  console.log('something went wrong: ' + error);
+});
