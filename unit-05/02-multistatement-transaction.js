c67708fa87828bc858a6d1eb9e1719fc501cdf76
Dave Cassel <dmcassel@gmail.com>
2016-02-19 12:40:23 -0500

    refactor code

--- unit-05/02-multistatement-transaction.js
+++ unit-05/02-multistatement-transaction.js
@@ -11,55 +11,70 @@ var db = ml.createDatabaseClient(conn);
 var pb = ml.patchBuilder;
 
 var jsonURI = '/image/01.JPG.json';
-var binaryURI = null;
-var updatedBinaryURI = null;
+
+// Change the 'binary' JSON property
+function updateJSONdoc(binaryURI, updatedBinaryURI, transactionID) {
+  // patch the JSON document
+
+  console.log('changing ' + binaryURI + ' to ' + updatedBinaryURI);
+  return db.documents.patch({
+    uri: jsonURI,
+    operations: pb.replace('/binary', updatedBinaryURI),
+    txid: transactionID
+  }).result();
+
+}
+
+function getBinaryImage(binaryURI, transactionID) {
+  // get the binary image
+  console.log('retrieving ' + binaryURI);
+  return db.documents.read({uris: binaryURI, categories: ['content', 'collections']}).result();
+}
+
+function insertBinary(binaryDocDesc, updatedBinaryURI, transactionID) {
+  // Insert the binary image at its new URI
+  console.log('inserting image at ' + updatedBinaryURI);
+
+  return db.documents.write({
+    documents: {
+      uri: updatedBinaryURI,
+      contentType: binaryDocDesc.contentType,
+      collections: binaryDocDesc.collections,
+      content: binaryDocDesc.content
+    },
+    txid: transactionID
+  }).result();
+}
+
+function deleteBinary(binaryURI, transactionID) {
+  // Remove the original binary image
+  console.log('removing image from ' + binaryURI);
+  db.documents.remove({ uris: binaryURI, txid: transactionID}).result();
+}
 
 // Create a transaction
 db.transactions.open(true).result()
   .then(function(txResponse) {
+    var transactionID = txResponse;
     // Search for all the JSON image descriptions
     db.documents.read(jsonURI).result()
-      .then(function(doc) {
-        // patch the JSON document
-        binaryURI = doc[0].content.binary;
-        updatedBinaryURI = binaryURI.toLowerCase();
-
-        console.log('patching ' + jsonURI);
-        return db.documents.patch({
-          uri: jsonURI,
-          operations: pb.replace('/binary', updatedBinaryURI),
-          txid: txResponse
-        }).result();
-      })
-      .then(function() {
-        // get the binary image
-        console.log('retrieving ' + binaryURI);
-        return db.documents.read({uris: binaryURI, categories: ['content', 'collections']}).result();
-      })
-      .then(function(doc) {
-        // Insert the binary image at its new URI
-        console.log('inserting image at ' + updatedBinaryURI);
-
-        return db.documents.write({
-          documents: {
-            uri: updatedBinaryURI,
-            contentType: doc[0].contentType,
-            collections: doc[0].collections,
-            content: doc[0].content
-          },
-          txid: txResponse
-        }).result();
-
-      })
-      .then(function() {
-        // Remove the original binary image
-        console.log('removing image from ' + binaryURI);
-        db.documents.remove({ uris: binaryURI, txid: txResponse}).result();
-      })
-      .then(function() {
-        // We've gotten this far, so everything worked. Commit.
-        console.log('done... commit!');
-        db.transactions.commit(txResponse);
+      .then(function(jsonDoc){
+        var binaryURI = jsonDoc[0].content.binary;
+        var updatedBinaryURI = binaryURI.toLowerCase();
+        getBinaryImage(binaryURI, transactionID)
+          .then(function(binaryDocDesc){
+            Promise.all(
+              [
+                updateJSONdoc(binaryURI, updatedBinaryURI, transactionID),
+                insertBinary(binaryDocDesc[0], updatedBinaryURI, transactionID),
+                deleteBinary(binaryURI, transactionID)
+              ])
+              .then(function(){
+                // We've gotten this far, so everything worked. Commit.
+                console.log('done... commit!');
+                db.transactions.commit(txResponse);
+              });
+          });
       })
       .catch(function(error) {
         // Something went wrong. Roll back the transaction.
